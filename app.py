bdad403541fd7e7549782f35b23dc4e37bd6fbf2
Yang Wang <penguinwang@smail.nchu.edu.tw>
2020-09-09 15:37:20 +0800

    update static

--- app.py
+++ app.py
@@ -6,19 +6,32 @@
 """
 from autotrading import data_handler
 from flask import Flask, jsonify, render_template, url_for
-from flask_googlecharts import GoogleCharts, BarChart, MaterialLineChart
-from flask_googlecharts.utils import prep_data
-
+import pandas as pd
+# from flask_googlecharts import GoogleCharts, BarChart, MaterialLineChart
+# from flask_googlecharts.utils import prep_data
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    df = data_handler.read_stock_table_from_db("GOOG")
-    data = df.to_dict(orient='records')
-    return render_template('index.html', {'data': data})
+    return render_template('index.html')
 
+@app.route('/data', methods=["GET"])
+def request():
+    #data = pd.read_json("./GOOG.json", orient="split")
+    choose_cols = ["Close"]
+    df = data_handler.read_stock_table_from_db("GOOG")[choose_cols]
+    df = df.reset_index(drop=False, inplace=False)
+    df["Date"] = df["Date"].apply(lambda x: pd.to_datetime(x).date())
 
-if __name__ == '__main__':
+    list_data = df.values.tolist()
+    # Add column name
+    list_data.insert(0, df.columns.tolist())
+    json_data = {
+        "data": list_data
+    }
+
+    return jsonify(json_data)
+
+if __name__ == "__main__":
     app.run(debug=True)
